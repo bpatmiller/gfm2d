f0bb6219f0f2167a6e67353ad66f36975fd3cd5a
Brendan Miller <brendan@Brendans-MBP.attlocal.net>
2019-08-16 17:29:24 -0500

    more math skeleton code

---
+++ src/calculus_2.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <glm/glm.hpp>
+
+/** returns the central differenced divergence */
+float divergence(Array2f& field, vec2 world_position) {
+    return 0;
+}
+
+/** returns the central differenced gradient of a point in space, */
+vec2 gradient(Array2f& field, vec2 world_position) {
+    return vec2(0);
+}
+
