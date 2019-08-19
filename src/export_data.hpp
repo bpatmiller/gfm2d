e7cbf390b7caed1f50cf9bcf362f6f4e8116771e
brendan miller <brendanmiller.ca@gmail.com>
2019-08-18 19:56:29 -0500

    changed default settings to air bubble

--- src/export_data.hpp
+++ src/export_data.hpp
@@ -26,7 +26,7 @@ void export_velocity(VelocityField &vel, Array2f phi, float time,
  * are guaranteed both no overlaps (because at most 1 is negative) and no gaps
  * (because we will never have no gaps).
  * */
-void export_fluid_ids(Array2f& p, std::vector<Fluid> &fluids, float time,
+void export_fluid_ids(Array2f &p, std::vector<Fluid> &fluids, float time,
                       int frame_number) {
   std::fstream fluid_id_file("plot/data/phi.txt",
                              fluid_id_file.out | fluid_id_file.app);
@@ -42,14 +42,16 @@ void export_fluid_ids(Array2f& p, std::vector<Fluid> &fluids, float time,
         continue;
       vec2 ij = it.ij();
       vec2 wp = f.phi.worldspace_of(ij);
-      fluid_id_file << wp.x << "\t" << wp.y << "\t" << *it << "\t" << n << "\t" << p(ij) << "\n";
+      fluid_id_file << wp.x << "\t" << wp.y << "\t" << *it << "\t" << n << "\t"
+                    << p(ij) << "\n";
     }
   }
   fluid_id_file.close();
 }
 
-void export_simulation_data(Array2f& p, VelocityField vel, std::vector<Fluid> &sim,
-                            float time, int frame_number) {
+void export_simulation_data(Array2f &p, VelocityField vel,
+                            std::vector<Fluid> &sim, float time,
+                            int frame_number) {
   std::printf("exporting frame %i at time %f\n", frame_number, time);
   export_fluid_ids(p, sim, time, frame_number);
   export_velocity(vel, sim[0].phi, time, frame_number);
