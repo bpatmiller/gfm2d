e432ed8603ccc2db2992128042b667acffefdf32
brendan miller <brendanmiller.ca@gmail.com>
2019-08-27 01:04:29 -0500

    fixed cmake if

--- test/test_calculus.cpp
+++ test/test_calculus.cpp
@@ -2,10 +2,6 @@
 
 #include "calculus.hpp"
 
-TEST(FirstParam, rk4) {
-    EXPECT_EQ(1,1);
-}
+TEST(FirstParam, rk4) { EXPECT_EQ(1, 1); }
 
-TEST(FirstParam, forward_euler) {
-    EXPECT_EQ(1,1);
-}
+TEST(FirstParam, forward_euler) { EXPECT_EQ(1, 1); }
